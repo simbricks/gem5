1f8d9bfdad81644e405373bd651d4eb0decd2dd8
Antoine Kaufmann <antoinek@mpi-sws.org>
2023-05-12 13:34:19 +0200

    arch-x86,dev: Pc/Southbridge connect to separate dma/pio/int bus
    Simply adds additional optional parameters to the attachIO function.
    
    Change-Id: I9d2c199513993cd8c351fc47599fbcb5b2dcfd62

--- src/dev/x86/SouthBridge.py
+++ src/dev/x86/SouthBridge.py
@@ -83,7 +83,7 @@ class SouthBridge(SimObject):
     # IDE controller
     ide = X86IdeController(disks=[], pci_func=0, pci_dev=4)
 
-    def attachIO(self, bus, pci_bus, dma_ports):
+    def attachIO(self, bus, pci_bus, dma_ports, dma_bus=None, int_bus=None):
         # Route interrupt signals
         self.pic1.output = self.io_apic.inputs[0]
         self.pic2.output = self.pic1.inputs[2]
@@ -108,9 +108,15 @@ class SouthBridge(SimObject):
         self.pit.pio = bus.mem_side_ports
         self.speaker.pio = bus.mem_side_ports
         self.io_apic.pio = bus.mem_side_ports
-        self.io_apic.int_requestor = bus.cpu_side_ports
+
+        if int_bus is None:
+            int_bus = bus
+        self.io_apic.int_requestor = int_bus.cpu_side_ports
         self.msi.pio = bus.mem_side_ports
-        self.msi.int_requestor = bus.cpu_side_ports
+        self.msi.int_requestor = int_bus.cpu_side_ports
+
         # Connect PCI devices
         self.ide.pio = pci_bus.mem_side_ports
-        self.ide.dma = pci_bus.cpu_side_ports
+        if dma_bus is None:
+            dma_bus = pci_bus
+        self.ide.dma = dma_bus.cpu_side_ports
